d8ee387e090f477b545149a8daa84cb052106b71
Nadohoi <100991243+Nadohoi@users.noreply.github.com>
2022-07-31 17:04:41 +0800

    Optimizing code and increase performance
    Added threading to enable multi-threading tasks

--- Color detection.py
+++ Color detection.py
@@ -6,8 +6,30 @@ import threading
 # Specifying upper and lower ranges of color to detect in hsv format
 lower_orange = np.array([5, 100, 150])
 upper_orange = np.array([13, 225, 225]) # (These ranges will detect Orange)
-lower_green = np.array([60, 70, 60])
+lower_green = np.array([55, 65, 55])
 upper_green = np.array([85, 255, 255])  # (These ranges will detect Green)
+
+def get_orange():
+    mask = cv2.inRange(img, lower_orange, upper_orange) # Masking the image to find our color
+    mask_contours, hierarchy = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE) # Finding contours in mask image
+    
+    if len(mask_contours) != 0:
+        for mask_contour in mask_contours:
+            if cv2.contourArea(mask_contour) > 500:
+                    cv2.putText(video, "STOP", (cx + 200, cy + 230), 0, 1, (255, 255, 255), 4)
+                    x, y, w, h = cv2.boundingRect(mask_contour)
+                    cv2.rectangle(video, (x, y), (x + w, y + h), (0, 0, 255), 3) #drawing rectangle
+
+def get_green():
+    mask2 = cv2.inRange(img, lower_green, upper_green)
+    mask_contours2, hierarchy2 = cv2.findContours(mask2, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE) # Finding contours in mask image
+    
+    if len(mask_contours2) != 0:
+        for mask_contour in mask_contours2:
+            if cv2.contourArea(mask_contour) > 150:
+                cv2.putText(video, "GO", (cx + 200, cy + 230), 0, 1, (255, 255, 255), 4)
+                x, y, w, h = cv2.boundingRect(mask_contour)
+                cv2.rectangle(video, (x, y), (x + w, y + h), (0, 0, 255), 3) #drawing rectangle
 
 # Capturing webcam footage
 webcam_video = cv2.VideoCapture(0)
@@ -24,34 +46,23 @@ while True:
     
     success, video = webcam_video.read() # Reading webcam footage
     img = cv2.cvtColor(video, cv2.COLOR_BGR2HSV) # Converting BGR image to HSV format
-    height, width, _ = video.shape
+    height, width, _ = img.shape
     cx = int(width / 2)
     cy = int(height / 2)
     
-    mask = cv2.inRange(img, lower_orange, upper_orange) # Masking the image to find our color
-    mask2 = cv2.inRange(img, lower_green, upper_green)
-    mask_contours, hierarchy = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE) # Finding contours in mask image
-    mask_contours2, hierarchy2 = cv2.findContours(mask2, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE) # Finding contours in mask image
+    thread1 = threading.Thread(target=get_orange())
+    thread2 = threading.Thread(target=get_green())
     
-    # Finding position of all contours
-    if len(mask_contours) != 0:
-        for mask_contour in mask_contours:
-            if cv2.contourArea(mask_contour) > 250:
-                cv2.putText(video, "STOP", (cx + 200, cy + 230), 0, 1, (255, 255, 255), 4)
-                x, y, w, h = cv2.boundingRect(mask_contour)
-                cv2.rectangle(video, (x, y), (x + w, y + h), (0, 0, 255), 3) #drawing rectangle
+    thread1.start()
+    thread2.start()
     
-    if len(mask_contours2) != 0:
-        for mask_contour in mask_contours2:
-            if cv2.contourArea(mask_contour) > 250:
-                cv2.putText(video, "GO", (cx + 200, cy + 230), 0, 1, (255, 255, 255), 4)
-                x, y, w, h = cv2.boundingRect(mask_contour)
-                cv2.rectangle(video, (x, y), (x + w, y + h), (0, 0, 255), 3) #drawing rectangle
+    thread1.join()
+    thread2.join()
     
     # Draw framerate in corner of frame
     cv2.putText(video, 'FPS: {0:.2f}'.format(frame_rate_calc), (cx - 300 , cy + 230), 0, 1, (255,255,255), 4)          
 
-    cv2.imshow("Health Code Detector", video) # Displaying webcam image
+    cv2.imshow("window", video) # Displaying webcam image
     
     # Calculate framerate
     t2 = cv2.getTickCount()
