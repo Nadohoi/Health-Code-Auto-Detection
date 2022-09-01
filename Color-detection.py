ab07c177689edfc5369136000efd000584cd647d
Nadohoi <100991243+Nadohoi@users.noreply.github.com>
2022-09-01 13:55:14 +0800

    Update Color-detection.py

--- Color-detection.py
+++ Color-detection.py
@@ -1,5 +1,5 @@
 # Offical release by Nadohoi
-# Ver.1
+# Ver.1.1 developing
 
 # Importing all modules
 import cv2
@@ -7,10 +7,23 @@ import numpy as np
 import threading
 
 # Specifying upper and lower ranges of color to detect in hsv format
+lower_red = np.array([0, 60, 60])
+upper_red = np.array([6, 255, 255])     # (These rangers will detect Red)
 lower_orange = np.array([5, 100, 150])
 upper_orange = np.array([13, 225, 225]) # (These ranges will detect Orange)
 lower_green = np.array([55, 65, 55])
 upper_green = np.array([85, 255, 255])  # (These ranges will detect Green)
+
+def get_red():
+    mask = cv2.inRange(img, lower_orange, upper_orange) # Masking the image to find our color
+    mask_contours, hierarchy = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE) # Finding contours in mask image
+    
+    if len(mask_contours) != 0:
+        for mask_contour in mask_contours:
+            if cv2.contourArea(mask_contour) > 500:
+                    cv2.putText(video, "STOP", (cx + 200, cy + 230), 0, 1, (255, 255, 255), 4)
+                    x, y, w, h = cv2.boundingRect(mask_contour)
+                    cv2.rectangle(video, (x, y), (x + w, y + h), (0, 0, 255), 3) # Drawing rectangle
 
 def get_orange():
     mask = cv2.inRange(img, lower_orange, upper_orange) # Masking the image to find our color
@@ -53,14 +66,17 @@ while True:
     cx = int(width / 2)
     cy = int(height / 2)
     
-    thread1 = threading.Thread(target=get_orange())
-    thread2 = threading.Thread(target=get_green())
+    thread1 = threading.Thread(target=get_red())
+    thread2 = threading.Thread(target=get_orange())
+    thread3 = threading.Thread(target=get_green())
     
     thread1.start()
     thread2.start()
+    thread3.start()
     
     thread1.join()
     thread2.join()
+    thread3.join()
     
     # Draw framerate in corner of frame
     cv2.putText(video, "FPS: {0:.2f}".format(frame_rate_calc), (cx - 300 , cy + 230), 0, 1, (255,255,255), 4)          
