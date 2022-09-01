417b12406d7eac4356f44cd1f3059d5fe450102c
Nadohoi <100991243+Nadohoi@users.noreply.github.com>
2022-09-01 14:03:06 +0800

    Update release for ver 1.1
    Code optimizing.

--- Color-adjustment.py
+++ Color-adjustment.py
@@ -1,5 +1,5 @@
 # Offical release by Nadohoi
-# Ver.1
+# Ver 1.1
 
 # Importing all modules
 import cv2
@@ -36,18 +36,14 @@ cv2.createTrackbar('LowerbH', "colors", 55, 255, nothing)
 cv2.createTrackbar('LowerbS', "colors", 65, 255, nothing)
 cv2.createTrackbar('LowerbV', "colors", 55, 255, nothing)
 cv2.createTrackbar('UpperbH', "colors", 85, 255, nothing)
-cv2.createTrackbar('UpperbS', "colors", 255, 255, nothing)
-cv2.createTrackbar('UpperbV', "colors", 255, 255, nothing)
 
 while(1):
     lowerbH = cv2.getTrackbarPos('LowerbH', "colors")
     lowerbS = cv2.getTrackbarPos('LowerbS', "colors")
     lowerbV = cv2.getTrackbarPos('LowerbV', "colors")
     upperbH = cv2.getTrackbarPos('UpperbH', "colors")
-    upperbS = cv2.getTrackbarPos('UpperbS', "colors")
-    upperbV = cv2.getTrackbarPos('UpperbV', "colors")
 
-    img_target = cv2.inRange(img_original, (lowerbH, lowerbS, lowerbV), (upperbH, upperbS, upperbV))
+    img_target = cv2.inRange(img_original, (lowerbH, lowerbS, lowerbV), (upperbH, 255, 255))
     
     img_specifiedColor = cv2.bitwise_and(img_original, img_original, mask = img_target)
     cv2.imshow(winName, img_specifiedColor)
