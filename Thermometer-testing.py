6cf908008d2e9d20466aa1389a1b7164a3eb9a52
Nadohoi <100991243+Nadohoi@users.noreply.github.com>
2022-10-09 10:41:42 +0800

    Update Thermometer-testing.py

--- Thermometer-testing.py
+++ Thermometer-testing.py
@@ -6,10 +6,10 @@ import adafruit_mlx90614 # pip3 install adafruit-circuitpython-mlx90614
 import busio as io
 
 # Locating mlx90614's i2c
-i2c = io.I2C(board.SCL,board.SCA, frequency=100000)
+i2c = io.I2C(board.SCL,board.SDA, frequency=100000)
 mlx = adafruit_mlx90614.MLX90614(i2c)
 
 while True:
     obj_temp = mlx.object_temperature
     amb_temp = mlx.ambient_temperature
-    print("Obj: " + obj_temp + "Amb: " + amb_temp)
+    print("Obj: " + str(obj_temp) + " " "Amb: " + str(amb_temp))
