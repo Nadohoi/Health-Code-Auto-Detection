289848198553f86aa93f6a4535225fa902edfc76
Nadohoi <100991243+Nadohoi@users.noreply.github.com>
2022-10-12 22:13:23 +0800

    Add files via upload

--- testing.py
+++ testing.py
@@ -1,36 +1,46 @@
 from imutils.perspective import four_point_transform
 import cv2
 import numpy as np
-import sys
-import time
 
-def four_point(x, y):
-    global four_points
-    four_points.append([x, y])
+camera = cv2.VideoCapture(0)
 
 while True:
-    camera = cv2.VideoCapture(0)
     return_value, inputimage = camera.read()
-    if cv2.waitKey() == ord('q'):
+    cv2.imshow('img', inputimage)
+    if cv2.waitKey(1) == ord('q'):
         cv2.imwrite('poke.png', inputimage)
         break
 
-img_path = 'poke.png'
-image = cv2.imread("poke.png")
-det = cv2.QRCodeDetector()
-info, box_coordinates, _ = det.detectAndDecode(img_path)
-four_points = [tuple(box_coordinates[0][i]), tuple(box_coordinates[0][(i+1) % n])]
-
-cv2.namedWindow("img")
-
-while True:
-    cv2.imshow('img', image)
-    if cv2.waitKey() == ord('q'):
-        break
-
-# print(four_points)
-rect = four_point_transform(image, np.array(four_points))
-cv2.imwrite('poke_rect.png', rect)
-cv2.imshow("rect", rect)
-cv2.waitKey(0)
 cv2.destroyAllWindows()
+
+# 加载图片
+src_image = cv2.imread("poke.png")
+# 实例化
+qrcoder = cv2.QRCodeDetector()
+# qr检测并解码
+codeinfo, points, straight_qrcode = qrcoder.detectAndDecode(src_image)
+# 绘制qr的检测结果
+cv2.drawContours(src_image, [np.int32(points)], 0, (0, 0, 255), 2)
+
+cv2.imshow("result", src_image)
+cv2.waitKey(0)
+
+
+#rect = four_point_transform(src_image, np.int32(points))
+#cv2.imwrite('poke_rect.png', rect)
+#cv2.imshow("rect", rect)
+
+
+
+
+#cv2.namedWindow("img")
+
+#while True:
+    #cv2.imshow('img', image)
+    #if cv2.waitKey() == ord('q'):
+        #break
+
+
+#cv2.waitKey(0)
+#cv2.destroyAllWindows()
+        
